3fb095f5a5fd51d2f6403c459f743ce99668df9a
maelho <51026438+maelho@users.noreply.github.com>
2025-12-31 13:53:29 -0400

    chore: remove answer

--- src/game-helpers.ts
+++ src/game-helpers.ts
@@ -8,8 +8,6 @@ type ResultItems = {
 type LetterCounts = Record<string, number>
 
 export function checkGuess(guess: string, answer: string): ResultItems[] | null {
-  console.log({ answer })
-
   if (!guess) {
     return null
   }
